090580374ee459eaa42c6b507f30062ce52d7c3c
Brady Blair <bblair321@yahoo.comm>
2025-04-18 07:20:41 -0700

    Initial Commit

---
+++ spotify-wallpaper-grid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spotify-wallpaper-grid
+//
+//  Created by Brady Blair on 4/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spotify-wallpaper-grid/spotify_wallpaper_gridApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_wallpaper_gridApp.swift
+//  spotify-wallpaper-grid
+//
+//  Created by Brady Blair on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct spotify_wallpaper_gridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
